45cdbfce01d132d24abe109faeec1c2eff1621b2
Shalini Dangi <shalinidangi@ucla.edu>
2016-02-18 14:50:38 -0800

    added member to osprd_info struct (list of processes that are "following" a ramdisk)

--- lab2/osprd.c
+++ lab2/osprd.c
@@ -245,41 +245,44 @@ void deleteList(mlist_t* l)
 
 /* The internal representation of our device. */
 typedef struct osprd_info {
-	uint8_t *data;                  // The data array. Its size is
-	                                // (nsectors * SECTOR_SIZE) bytes.
+	uint8_t *data;                          // The data array. Its size is
+	                                        // (nsectors * SECTOR_SIZE) bytes.
 
-	osp_spinlock_t mutex;           // Mutex for synchronizing access to
-					// this block device
+	osp_spinlock_t mutex;                   // Mutex for synchronizing access to
+	                                        // this block device
 
-	unsigned ticket_head;		// Currently running ticket for
-					// the device lock
+	/********************TICKET LIST******************************************/
+	unsigned ticket_head;					// Currently running ticket for the device lock
 
-	unsigned ticket_tail;		// Next available ticket for
-					// the device lock
+	unsigned ticket_tail;					// Next available ticket for the device lock
 
-	ticketList_t tickets; //linked list of tickets
-	ticketList_t *first_ticket; //points to first-served ticket in queue
+	ticketList_t tickets; 					// linked list of tickets
+	ticketList_t *first_ticket; 			// points to first-served ticket in queue
 
-	wait_queue_head_t blockq;       // Wait queue for tasks blocked on
-					// the device lock
+	wait_queue_head_t blockq;       		// Wait queue for tasks blocked on the device lock
 
+	/**************************NOTIFICATION LIST******************************/
+	mlist_t notify_pids;					// linked list of processes that want to be notified
+	                                        // when there is a write to this ramdisk
+
+	/**************************DEADLOCK***************************************/
 	/* HINT: You may want to add additional fields to help
 	         in detecting deadlock. */
 
-	int write_locked;				// Should only ever be 0 or 1.
-									// Indicates whether the file is write locked
+	int write_locked;				        // Should only ever be 0 or 1.
+	                                        // Indicates whether the file is write locked
 
-	int write_lock_pid;				// PID of the process that holds the write lock
+	int write_lock_pid;			            // PID of the process that holds the write lock
 
-	int num_read_locks;				// Indicates the number of read locks on the file
+	int num_read_locks;			            // Indicates the number of read locks on the file
 
-	mlist_t read_lock_pids;			// Linked list of read lock pids
+	mlist_t read_lock_pids;			        // Linked list of read lock pids
+	
 	// The following elements are used internally; you don't need
 	// to understand them.
-	struct request_queue *queue;    // The device request queue.
-	spinlock_t qlock;		// Used internally for mutual
-	                                //   exclusion in the 'queue'.
-	struct gendisk *gd;             // The generic disk.
+	struct request_queue *queue;    	   // The device request queue.
+	spinlock_t qlock;			           // Used internally for mutual exclusion in the 'queue'.
+	struct gendisk *gd;             	   // The generic disk.
 } osprd_info_t;
 
 
